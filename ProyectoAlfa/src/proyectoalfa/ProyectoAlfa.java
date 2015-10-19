324d1c99c466a9f7c6a90c4200258a2d8e3e0d2f
manu <manu@manu-HP-Pavilion-g6-Notebook-PC>
2015-10-19 09:12:56 +0200

    Primer comit de Alfa

---
+++ ProyectoAlfa/src/proyectoalfa/ProyectoAlfa.java
@@ -0,0 +1,13 @@
+package proyectoalfa;
+
+/**
+ *
+ * @author manu
+ */
+public class ProyectoAlfa {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
